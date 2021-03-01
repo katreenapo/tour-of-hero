9db6bf87eb276e214889b49a2a6e1524d7f3ee0d
katreenapo <katja.djadechko1@gmail.com>
2021-03-01 20:32:34 +0200

    master

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss']
+})
+export class MessagesComponent implements OnInit {
+  messages: string[] = [];
+ 
+  add(message: string) {
+    this.messages.push(message);
+  }
+ 
+  clear() {
+    this.messages = [];
+  }
+
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
